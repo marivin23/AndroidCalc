4828242eae7f48e60f1097f674db272ef658d52d
Marius Irimie Victor <32970754+marivin23@users.noreply.github.com>
2018-04-13 01:06:01 -0700

    Add files via upload

--- ccalculator/app/src/main/java/com/cucc/ccalculator/Calculate.java
+++ ccalculator/app/src/main/java/com/cucc/ccalculator/Calculate.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 public class Calculate extends MainActivity {
 
-    private static int multiples = 1;
     static int result = 0;
 
     public void calculateArray(List<String> items) {
@@ -26,25 +25,34 @@ public class Calculate extends MainActivity {
 
                 switch (s) {
                     case "+": {
+                        if(items.size() - iterate == 2) {
+                            n1 = Integer.parseInt(items.get(iterate + 1));
+                            result = result + n1;
+                            break;
+                        }
+                        if (items.get(iterate + 2).equals("*") || items.get(iterate + 2).equals("/")) {
+                            break;
+                        }
                         n1 = Integer.parseInt(items.get(iterate + 1));
                         result = result + n1;
                         break;
                     }
                     case "-": {
+                        if(items.get(iterate+2).equals("*") || items.get(iterate+2).equals("/")) break;
                         n1 = Integer.parseInt(items.get(iterate + 1));
                         result = result - n1;
                         break;
                     }
                     case "*": {
-                        int secondaryResult = 0;
-                        if(iterate >= 2 ){
-                            if((!items.get(iterate - 2).equals("+") && !items.get(iterate - 2).equals("+"))){
-                               secondaryResult = Integer.parseInt(items.get(iterate-1)) * Integer.parseInt(items.get(iterate+1));
-                               result = secondaryResult + result;
-                            }
-                        } else {
-                            //result = result * Integer.parseInt(items.get(iterate+1));
+                        if(items.get(1).equals("*")||items.get(1).equals("/")) {
+                            result = result * Integer.parseInt(items.get(iterate+1));
+                            break;
                         }
+                        result = result + (Integer.parseInt(items.get(iterate-1)) * Integer.parseInt(items.get(iterate+1)));
+                        break;
+                    }
+                    case "/": {
+                        result = result + (Integer.parseInt(items.get(iterate-1)) / Integer.parseInt(items.get(iterate+1)));
                         break;
                     }
 
@@ -56,6 +64,11 @@ public class Calculate extends MainActivity {
 
     protected static int getResult(){
         return result;
+    }
+
+    private boolean hasIndex(int index){
+        if(index < items.size()) return false;
+        else return true;
     }
 
     protected static void setResult(){
